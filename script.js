37600a1fb42ac7558b422db3f9733294f17fb437
DanielaContrerasF <danycf98@gmail.com>
2025-03-18 19:44:30 -0600

    c

---
+++ script.js
@@ -0,0 +1,9 @@
+//Boton de White Lottus
+document.getElementById("botonjournaling").addEventListener("click", function() {
+    window.location.href = "https://proyecto-journaling.vercel.app/";
+});
+
+//Boton de journaling
+document.getElementById("botonwhitelotus").addEventListener("click", function() {
+    window.location.href = "https://m0-00n.github.io/Equipo-1/index.html";
+});
